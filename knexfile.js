b885bc048a817f66e391060e62218bb4cd171756
wanderblue <wanderblue691@gmail.com>
2019-09-08 15:58:43 -0700

    jaw darabase

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,16 @@
 // Update with your config settings.
 
 module.exports = {
+  
   client: 'mysql',
   connection: {
-    host: 'localhost',
+    host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
-    user: 'newuser',
-    password: '',
-    database: 'db_1234chan',
-  // database: 'tck253fslt3p0jo8',
-    debug: ['ComQueryPacket', 'RowDataPacket']
+    user: 'esfry26eyvbg75yo',
+    password: 'ko6r9upvukqe7pni',
+  //  database: 'db_1234chan',
+   database: 'mysql://esfry26eyvbg75yo:ko6r9upvukqe7pni@l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vdncpozy5fmpn2ot',
+    //debug: ['ComQueryPacket', 'RowDataPacket']
   }
+  
 }
